5ccc59684227077a8732961dde860117af703f53
EZaiger <elisabeth.zaiger@oit.edu>
2022-10-24 19:40:27 -0700

    completed About section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,26 +3,26 @@ import React from "react";
 export default function Footer() {
   return (
     <>
-      <div className="container text-center ">
+      <div className="container text-center fixed-bottom">
         <div className="row justify-content-center">
           <div className="col-2">
             <div className="p-3">
               <a href="https://github.com/ezaiger">
-                <i className="fa-brands fa-github-alt"></i>
+                <i className="fa-brands fa-github-alt fa-2x"></i>
               </a>
             </div>
           </div>
           <div className="col-2">
             <div className="p-3">
               <a href="https://www.linkedin.com/in/elisabethzaiger/">
-                <i className="fa-brands fa-linkedin-in"></i>
+                <i className="fa-brands fa-linkedin-in fa-2x"></i>
               </a>
             </div>
           </div>
           <div className="col-2">
             <div className="p-3">
               <a href="https://elisabethzaiger.com">
-                <i className="fa-solid fa-camera-retro"></i>
+                <i className="fa-solid fa-camera-retro fa-2x"></i>
               </a>
             </div>
           </div>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React from "react";
+import ezaiger from "../assets/images/Ezaiger.jpg";
 
 export default function About() {
   return (
     <div>
-      <h2>About Page</h2>
+      <h2>About</h2>
+      <img
+        src={ezaiger}
+        className="img-thumbnail"
+        alt="Elisabeth wearing a purple jacket"
+      ></img>
+      <p className="py-1">
+        My name is Elisabeth and I was born and raised throughout Oregon. I
+        graduated from Southern Oregon University with dual degrees:{" "}
+        <em>Bachelor of Science in Emerging Digital Media</em> and{" "}
+        <em>Bachelor of Fine Arts in Studio Art</em> with a photography,
+        illustration and graphic design emphasis. In addition to my degrees, I
+        dual minored in <em>Art History</em> and{" "}
+        <em>Film, Television, and Convergent Media</em>. I consider myself a
+        multipotentialite and am drawn to the fields of graphic design,
+        photography, software development, interior design, professional
+        organizing and entrepreneurship.
+      </p>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        My career objective is to utilize my strengths, be creative and feel challenged
+        in whatever role I am designated. I am an organized, communicative and
+        detail-oriented coder who contributed to past projects by finding ways
+        to streamline coding. For example, I created the wireframe and organized
+        the Kanban board in every group project I was a member of. While I am
+        able to work well as a team, it is vital for me to know my own roles and
+        responsibilities in order to thrive when I work independently. Thus, I
+        worked diligently to make tickets and comments in my code detailed for
+        my teammates and other developers to review and easily comprehend to
+        allow easier integration when code was eventually merged. In my previous
+        occupations, I was no stranger to adapting to new programs, policies or
+        procedures. Being in a coding bootcamp allowed me to experience
+        first-hand how technology is constantly changing and evolving. I am
+        excited to grow as a developer and be challenged by this ever-evolving
+        field and build confidence in my abilities through self-study and
+        on-the-job experience.
       </p>
     </div>
   );
